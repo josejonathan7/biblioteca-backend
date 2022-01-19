4165104c681e0138ac7c72099c607a24163da2d0
jose jonathan <josejonathan689@gmail.com>
2022-01-18 21:49:30 -0300

    conclusao dos testes unitarios

--- __tests__/unit/workWriters.test.ts
+++ __tests__/unit/workWriters.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it, beforeEach } from "@jest/globals";
 import faker from "@faker-js/faker";
 import WorkWriteService from "../../src/services/WorksWritesServices";
+import { getCustomRepository } from "typeorm";
+import WorkRepositorie from "../../src/repositories/workREpositorie";
 
 describe("#Unitary test", () => {
 	let data: {
@@ -24,10 +26,151 @@ describe("#Unitary test", () => {
 		workWriteService = new WorkWriteService();
 	});
 
-	it("#create Work", async () => {
-		const created = await workWriteService.createWork(data);
+	afterEach(async () => {
+		const workWriteRepositorie = getCustomRepository(WorkRepositorie, "sqliteDb");
 
-		expect(created).toMatchObject({...data});
+		await workWriteRepositorie.delete({});
 	});
 
+	it("->Should create Work", async () => {
+		const created = await workWriteService.createWork(data);
+
+		expect(created).toMatchObject({
+			...data,
+			author: {}
+		});
+	});
+
+	it("->Should not create work with existent image", async () => {
+		const dataTwo = {
+			...data,
+			title: faker.name.title()
+		};
+
+		await workWriteService.createWork(data);
+
+		try {
+
+			await workWriteService.createWork(dataTwo);
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("The Work image already exist");
+		}
+
+	});
+
+	it("->Should not create work with existent title", async () => {
+		const dataTwo = {
+			...data,
+			image: faker.image.abstract()
+		};
+
+		await workWriteService.createWork(data);
+
+		try {
+
+			await workWriteService.createWork(dataTwo);
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("The Work title already exist");
+		}
+
+	});
+
+	it("->Should get all works", async () => {
+		await workWriteService.createWork(data);
+
+		const dataTwo = {
+			...data,
+			title: faker.name.title(),
+			image: faker.image.animals(),
+		};
+
+		const dataThree = {
+			title: faker.name.title(),
+			publishingCompany: faker.company.companyName(),
+			image: faker.image.avatar(),
+			author: faker.name.firstName()
+		};
+
+		await workWriteService.createWork(dataTwo);
+		await workWriteService.createWork(dataThree);
+
+		const getWorks = await workWriteService.getAll();
+
+		expect(getWorks[0]).toMatchObject({
+			...data,
+			author: {}
+		});
+	});
+
+	it("->Should not get with empty works", async () => {
+		try {
+			await workWriteService.getAll();
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("Works not found");
+		}
+	});
+
+	it("->Should update work", async() => {
+		const createWork = await workWriteService.createWork(data);
+
+		const id = createWork.id;
+		const updateObject = {
+			title: faker.name.title(),
+			publishingCompany: faker.company.companyName(),
+			image: faker.image.abstract(),
+			author: faker.name.firstName()
+		};
+
+		const updatedWork = await workWriteService.updateWork(updateObject, id);
+
+		expect(updatedWork).toBeTruthy();
+	});
+
+	it("->Should not update with incorrect Id", async () =>  {
+		await workWriteService.createWork(data);
+
+		const id = "asdasd2eqdasdad";
+
+		const updateObject = {
+			title: faker.name.title(),
+			publishingCompany: faker.company.companyName(),
+			image: faker.image.abstract(),
+			author: faker.name.firstName()
+		};
+		try {
+			await workWriteService.updateWork(updateObject, id);
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("Update failed");
+		}
+	});
+
+	it("->Should delete work",async () => {
+		const createWork = await workWriteService.createWork(data);
+
+		const id = createWork.id;
+
+		const deletedProduct = await workWriteService.deleteWork(id);
+
+		expect(deletedProduct).toBeTruthy();
+	});
+
+	it("->Should not delete with incorrect Id", async () => {
+		await workWriteService.createWork(data);
+
+		const id = "dasdasdasdasdasdas";
+
+		try {
+			await workWriteService.deleteWork(id);
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			expect(error.message).toBe("Work not found to be deleted");
+		}
+	});
 });
--- src/services/WorksWritesServices.ts
+++ src/services/WorksWritesServices.ts
@@ -1,4 +1,4 @@
-import { Connection, createConnection, getConnection, getCustomRepository } from "typeorm";
+import { Connection, createConnection, getConnection } from "typeorm";
 import {instanceToPlain} from "class-transformer";
 import AuthorRepositorie from "../repositories/authorRepositorie";
 import WorkRepositorie from "../repositories/workREpositorie";
@@ -23,6 +23,17 @@ export default class WorksWritesService {
 		const workRepositorie = await this.connection.then(con => con.getCustomRepository(WorkRepositorie));
 		const authorRepositorie = await this.connection.then(con => con.getCustomRepository(AuthorRepositorie));
 
+		const titleExists = await workRepositorie.find({where: {title}});
+
+		if(titleExists.length > 0) {
+			throw new Error("The Work title already exist");
+		}
+
+		const imageExist = await workRepositorie.find({where: {image}});
+
+		if(imageExist.length > 0) {
+			throw new Error("The Work image already exist");
+		}
 
 		const authorExists = await authorRepositorie.find({where: {name: author}});
 
@@ -48,6 +59,65 @@ export default class WorksWritesService {
 		const createdWork = await workRepositorie.save(saveWork);
 
 		return instanceToPlain(createdWork);
+	}
+
+	async getAll() {
+		const workWriteRepositorie = await this.connection.then(con => con.getCustomRepository(WorkRepositorie));
+
+		const worksFind = await workWriteRepositorie.find();
+
+		if(worksFind.length < 1) {
+			throw new Error("Works not found");
+		}
+
+		return instanceToPlain(worksFind);
+	}
+
+	async updateWork({title, publishingCompany, image, author}: Work, id: string){
+		const workRepositorie = await this.connection.then(con => con.getCustomRepository(WorkRepositorie));
+		const authorRepositorie = await this.connection.then(con => con.getCustomRepository(AuthorRepositorie));
+
+		const authorExists = await authorRepositorie.find({where: {name: author}});
+
+		if(authorExists.length < 1) {
+			await authorRepositorie.save({
+				name: author
+			});
+		}
+
+		const authorFind = await authorRepositorie.find({where: {name: author}});
+		const authorId = instanceToPlain(authorFind);
+
+		const updatedWork = await workRepositorie.update(id, {
+			title,
+			publishingCompany,
+			image,
+			author: {
+				id: authorId[0].id
+			}
+		});
+
+		const affected: number = updatedWork.affected ? updatedWork.affected : 0;
+
+		if(affected < 1) {
+			throw new Error("Update failed");
+		}
+
+		return updatedWork.affected;
+	}
+
+	async deleteWork(id: string) {
+		const workRepositorie = await this.connection.then(con => con.getCustomRepository(WorkRepositorie));
+
+		const deletedWork = await workRepositorie.delete(id);
+
+		const affected: number = deletedWork.affected ? deletedWork.affected : 0;
+
+		if(affected < 1) {
+			throw new Error("Work not found to be deleted");
+		}
+
+		return deletedWork.affected;
 	}
 
 	async load() {
